b19fc59a4e2cdea38554878e68062ad27e97c3df
Zwei <zwei.elen@outlook.com>
2023-07-03 23:55:39 +0800

    submit 274 h_index

---
+++ problem/274_h-index/case.go
@@ -0,0 +1,57 @@
+package h_index
+
+import (
+	"testing"
+
+	"leetcode/tests"
+)
+
+type Input struct {
+	citations []int
+}
+
+type Output int
+
+var cases = func() []tests.Case[Input, Output] {
+	return []tests.Case[Input, Output]{
+		{Input: Input{citations: []int{3, 0, 6, 1, 5}}, Except: 3},
+		{Input: Input{citations: []int{1, 3, 1}}, Except: 1},
+	}
+}
+
+type Func func(citations []int) int
+
+var funcs = tests.NewFuncWithAdaptor(adaptor)
+
+func adaptor(f Func) func(in Input) Output {
+	return func(in Input) Output {
+		var citations = make([]int, len(in.citations))
+		copy(citations, in.citations)
+
+		return Output(f(
+			citations,
+		))
+	}
+}
+
+func AddCases(c func() []tests.Case[Input, Output]) {
+	_cases := cases()
+	cases = func() []tests.Case[Input, Output] {
+		return append(_cases, c()...)
+	}
+}
+
+func AddFunc(f ...Func) {
+	funcs = append(funcs, tests.NewFuncWithAdaptor(adaptor, f...)...)
+}
+
+func Unit(t *testing.T) {
+	tests.Unit(t, cases, funcs...)
+}
+
+var checkResult = true
+
+func Bench(b *testing.B) {
+	checkResult = false
+	tests.Bench(b, cases, funcs...)
+}
