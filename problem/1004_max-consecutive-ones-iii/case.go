fa0b60b9e637c9c260fb77feda2f5dd18327a65c
Zwei <zwei.elen@outlook.com>
2023-07-23 16:58:18 +0800

    submit: 1004. max-consecutive-ones-iii

---
+++ problem/1004_max-consecutive-ones-iii/case.go
@@ -0,0 +1,79 @@
+package max_consecutive_ones_iii
+
+import (
+	"reflect"
+	"testing"
+
+	"leetcode/tests"
+)
+
+type Input struct {
+	nums []int
+	k    int
+}
+
+type Output int
+
+var cases = func() []tests.Case[Input, Output] {
+	return []tests.Case[Input, Output]{
+		{
+			Input: Input{
+				nums: []int{1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0},
+				k:    2,
+			},
+			Except: 6,
+		},
+		{
+			Input: Input{
+				nums: []int{0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 1, 1, 0, 0, 0, 1, 1, 1, 1},
+				k:    3,
+			},
+			Except: 10,
+		},
+	}
+}
+
+type Func func(nums []int, k int) int
+
+var funcs = tests.NewFuncWithAdaptor(adaptor)
+
+func adaptor(f Func) func(in Input) Output {
+	return func(in Input) Output {
+		return Output(f(
+			in.nums,
+			in.k,
+		))
+	}
+}
+
+func AddCases(c func() []tests.Case[Input, Output]) {
+	_cases := cases()
+	cases = func() []tests.Case[Input, Output] {
+		return append(_cases, c()...)
+	}
+}
+
+func AddFunc(f ...Func) {
+	funcs = append(funcs, tests.NewFuncWithAdaptor(adaptor, f...)...)
+}
+
+func Equal(x, y Output) bool {
+
+	return reflect.DeepEqual(x, y)
+}
+
+func Unit(t *testing.T) {
+	tests.Unit(t, tests.Test[Input, Output]{
+		Solution: funcs,
+		Cases:    cases,
+		IsEqual:  Equal,
+	})
+}
+
+func Bench(b *testing.B) {
+	tests.Bench(b, tests.Test[Input, Output]{
+		Solution: funcs,
+		Cases:    cases,
+		IsEqual:  Equal,
+	})
+}
