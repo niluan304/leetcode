217470a310be26bdb2e77611e6df303558747dbd
Zwei <zwei.elen@outlook.com>
2023-07-27 00:01:52 +0800

    submit: 424. longest-repeating-character-replacement

---
+++ problem/424_longest-repeating-character-replacement/case.go
@@ -0,0 +1,73 @@
+package longest_repeating_character_replacement
+
+import (
+	"reflect"
+	"testing"
+
+	"leetcode/tests"
+)
+
+type Input struct {
+	s string
+	k int
+}
+
+type Output int
+
+var cases = func() []tests.Case[Input, Output] {
+	return []tests.Case[Input, Output]{
+		{
+			Input:  Input{s: "ABAB", k: 2},
+			Except: 4,
+		},
+		{
+			Input:  Input{s: "AABABBA", k: 1},
+			Except: 4,
+		},
+	}
+}
+
+type Func func(s string, k int) int
+
+var funcs = tests.NewFuncWithAdaptor(adaptor)
+
+func adaptor(f Func) func(in Input) Output {
+	return func(in Input) Output {
+		return Output(f(
+			in.s,
+			in.k,
+		))
+	}
+}
+
+func AddCases(c func() []tests.Case[Input, Output]) {
+	_cases := cases()
+	cases = func() []tests.Case[Input, Output] {
+		return append(_cases, c()...)
+	}
+}
+
+func AddFunc(f ...Func) {
+	funcs = append(funcs, tests.NewFuncWithAdaptor(adaptor, f...)...)
+}
+
+func Equal(x, y Output) bool {
+
+	return reflect.DeepEqual(x, y)
+}
+
+func Unit(t *testing.T) {
+	tests.Unit(t, tests.Test[Input, Output]{
+		Solution: funcs,
+		Cases:    cases,
+		IsEqual:  Equal,
+	})
+}
+
+func Bench(b *testing.B) {
+	tests.Bench(b, tests.Test[Input, Output]{
+		Solution: funcs,
+		Cases:    cases,
+		IsEqual:  Equal,
+	})
+}
