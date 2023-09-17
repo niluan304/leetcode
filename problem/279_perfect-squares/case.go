620b65c72a7e0d3d0dcd874ae8cfcb3a10444845
Zwei <zwei.elen@outlook.com>
2023-09-17 21:44:11 +0800

    submit: 279. perfect-squares

---
+++ problem/279_perfect-squares/case.go
@@ -0,0 +1,75 @@
+package perfect_squares
+
+import (
+	"reflect"
+	"testing"
+
+	"leetcode/tests"
+)
+
+type Input struct {
+	n int
+}
+
+type Output int
+
+var cases = func() []tests.Case[Input, Output] {
+	return []tests.Case[Input, Output]{
+		{
+			Input: Input{
+				n: 12,
+			},
+			Except: 3,
+		},
+		{
+			Input: Input{
+				n: 13,
+			},
+			Except: 2,
+		},
+	}
+}
+
+type Func func(n int) int
+
+var funcs = tests.NewFuncWithAdaptor(adaptor)
+
+func adaptor(f Func) func(in Input) Output {
+	return func(in Input) Output {
+		return Output(f(
+			in.n,
+		))
+	}
+}
+
+func AddCases(c func() []tests.Case[Input, Output]) {
+	_cases := cases()
+	cases = func() []tests.Case[Input, Output] {
+		return append(_cases, c()...)
+	}
+}
+
+func AddFunc(f ...Func) {
+	funcs = append(funcs, tests.NewFuncWithAdaptor(adaptor, f...)...)
+}
+
+func Equal(x, y Output) bool {
+
+	return reflect.DeepEqual(x, y)
+}
+
+func Unit(t *testing.T) {
+	tests.Unit(t, tests.Test[Input, Output]{
+		Solution: funcs,
+		Cases:    cases,
+		IsEqual:  Equal,
+	})
+}
+
+func Bench(b *testing.B) {
+	tests.Bench(b, tests.Test[Input, Output]{
+		Solution: funcs,
+		Cases:    cases,
+		IsEqual:  Equal,
+	})
+}
