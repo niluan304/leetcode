25d2901607c1751036b76766ad5f6386cdbbcd97
Zwei <zwei.elen@outlook.com>
2023-10-09 21:24:52 +0800

    submit: 2034. stock-price-fluctuation

---
+++ problem/2034_stock-price-fluctuation/case.go
@@ -0,0 +1,61 @@
+package stock_price_fluctuation
+
+import (
+	"reflect"
+	"testing"
+
+	"leetcode/tests"
+)
+
+type Input struct {
+}
+
+type Output bool
+
+var cases = func() []tests.Case[Input, Output] {
+	return []tests.Case[Input, Output]{
+		// TODO add question case
+	}
+}
+
+type Func func() bool
+
+var funcs = tests.NewFuncWithAdaptor(adaptor)
+
+func adaptor(f Func) func(in Input) Output {
+	return func(in Input) Output {
+		return Output(f())
+	}
+}
+
+func AddCases(c func() []tests.Case[Input, Output]) {
+	_cases := cases()
+	cases = func() []tests.Case[Input, Output] {
+		return append(_cases, c()...)
+	}
+}
+
+func AddFunc(f ...Func) {
+	funcs = append(funcs, tests.NewFuncWithAdaptor(adaptor, f...)...)
+}
+
+func Equal(x, y Output) bool {
+
+	return reflect.DeepEqual(x, y)
+}
+
+func Unit(t *testing.T) {
+	tests.Unit(t, tests.Test[Input, Output]{
+		Solution: funcs,
+		Cases:    cases,
+		IsEqual:  Equal,
+	})
+}
+
+func Bench(b *testing.B) {
+	tests.Bench(b, tests.Test[Input, Output]{
+		Solution: funcs,
+		Cases:    cases,
+		IsEqual:  Equal,
+	})
+}
