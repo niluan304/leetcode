c0d3627c49bce7cc5b373324faa28bf3289e17fb
Zwei <zwei.elen@outlook.com>
2023-12-13 09:49:44 +0800

    submit: 222. count-complete-tree-nodes

---
+++ problem/222_count-complete-tree-nodes/solution.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"sort"
+
+	. "github.com/EndlessCheng/codeforces-go/leetcode/testutil"
+)
+
+func max(x, y int) int {
+	if x > y {
+		return x
+	}
+	return y
+}
+
+func min(x, y int) int {
+	if x < y {
+		return x
+	}
+	return y
+}
+
+func abs(x int) int {
+	if x < 0 {
+		return -x
+	}
+	return x
+}
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func countNodes(root *TreeNode) int {
+	count := 0
+	var dfs func(node *TreeNode)
+	dfs = func(node *TreeNode) {
+		if node == nil {
+			return
+		}
+
+		count++
+		dfs(node.Right)
+		dfs(node.Left)
+	}
+
+	dfs(root)
+	return count
+}
+
+func countNodes2(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	height := 0
+	for node := root; node.Left != nil; node = node.Left {
+		height++
+	}
+
+	return sort.Search(1<<(height+1), func(i int) bool {
+		if i <= 1<<height { // 用于第一次二分，让边界直接跳到最左边的叶子的节点
+			return false
+		}
+		bits := 1 << (height - 1)
+		node := root
+		for node != nil && bits > 0 {
+			if bits&i == 0 {
+				node = node.Left
+			} else {
+				node = node.Right
+			}
+			bits >>= 1
+		}
+		return node == nil
+	}) - 1
+}
