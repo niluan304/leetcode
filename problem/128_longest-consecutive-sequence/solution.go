5f2a311da7478f544dda8c0860681820e675950f
Zwei <zwei.elen@outlook.com>
2024-01-29 13:59:09 +0800

    update: 128. longest-consecutive-sequence

--- problem/128_longest-consecutive-sequence/solution.go
+++ problem/128_longest-consecutive-sequence/solution.go
@@ -4,14 +4,18 @@ import (
 	"sort"
 )
 
-func longestConsecutive(nums []int) int {
+// 排序
+// - 时间复杂度：$\mathcal{O}(n\log n)$。
+// - 空间复杂度：$\mathcal{O}(1)$。
+// Deprecated: 超时
+func sortInt(nums []int) int {
 	if len(nums) == 0 {
 		return 0
 	}
 
 	sort.Ints(nums)
 
-	var max = 1
+	var mx = 1
 	var cur = 1
 
 	for i := 1; i < len(nums); i++ {
@@ -24,10 +28,35 @@ func longestConsecutive(nums []int) int {
 			cur = 1
 		}
 
-		if max < cur {
-			max = cur
+		if mx < cur {
+			mx = cur
 		}
 	}
 
-	return max
+	return mx
+}
+
+// 1e-9 <= nums[i] <= 1e9
+// len(nums) <= 1e5
+// 哈希表
+//
+// - 时间复杂度：$\mathcal{O}(n)$。
+// - 空间复杂度：$\mathcal{O}(n)$。
+func longestConsecutive(nums []int) int {
+	set := map[int]bool{}
+	for _, num := range nums {
+		set[num] = true
+	}
+	ans := 0
+	for num, _ := range set {
+		if set[num-1] {
+			continue
+		}
+		count := 1
+		for set[num+count] {
+			count++
+		}
+		ans = max(ans, count)
+	}
+	return ans
 }
--- problem/128_longest-consecutive-sequence/solution_test.go
+++ problem/128_longest-consecutive-sequence/solution_test.go
@@ -1,30 +1,55 @@
 package main
 
 import (
-	"reflect"
-	"runtime"
-	"strings"
+	"math/rand"
 	"testing"
 
-	"github.com/EndlessCheng/codeforces-go/leetcode/testutil"
+	"github.com/niluan304/leetcode/tests"
 )
 
 func Test_longest_consecutive_sequence(t *testing.T) {
 	targetCaseNum := 0 // -1
 
-	fs := []interface{}{
+	fs := []func(nums []int) int{
+		sortInt,
 		longestConsecutive,
 	}
 
 	for _, f := range fs {
-		name := runtime.FuncForPC(reflect.ValueOf(f).Pointer()).Name()
-		i := strings.LastIndex(name, ".")
-
-		t.Run(name[i+1:], func(t *testing.T) {
-			err := testutil.RunLeetCodeFuncWithFile(t, f, "sample.txt", targetCaseNum)
-			if err != nil {
-				t.Error(err)
-			}
-		})
+		err := tests.RunFunc(t, f, samples, targetCaseNum)
+		if err != nil {
+			t.Error(err)
+		}
 	}
+}
+
+var samples = `
+[100,4,200,1,3,2]
+4
+
+[0,3,7,2,5,8,4,6,0,1]
+9
+
+
+
+`
+
+func Test_validate(t *testing.T) {
+	for i := 0; i < 100000; i++ {
+		nums := generate()
+		except := sortInt(nums)
+		err := tests.Validate(nums, except, longestConsecutive)
+		if err != nil {
+			t.Error(err)
+		}
+	}
+}
+
+func generate() []int {
+	var n = rand.Intn(1000) + 1
+	var nums = make([]int, 1000)
+	for i, _ := range nums {
+		nums[i] = rand.Intn(n)
+	}
+	return nums
 }
