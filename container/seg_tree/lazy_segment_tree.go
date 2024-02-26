32e5d6772429b87c1245e5fe95a8024e624a0991
Zwei <zwei.elen@outlook.com>
2024-02-26 22:14:28 +0800

    copypasta: update segment tree

--- container/seg_tree/lazy_segment_tree.go
+++ container/seg_tree/lazy_segment_tree.go
@@ -1,26 +1,26 @@
 package seg_tree
 
-// NewLazySegmentTree
+// NewLazySegmentTreeWithNums
 // 根据数组初始化线段树
-func NewLazySegmentTree(nums []int) *LazySegmentTree {
+func NewLazySegmentTreeWithNums(nums []int) *LazySegmentTree {
 	root := &LazySegmentTree{}
-	var dfs func(st *LazySegmentTree, left, right int)
-	dfs = func(st *LazySegmentTree, left, right int) {
-		*st = LazySegmentTree{
-			left:   left,
-			right:  right,
-			lChild: &LazySegmentTree{},
-			rChild: &LazySegmentTree{},
+	var dfs func(node *LazySegmentTree, left, right int)
+	dfs = func(node *LazySegmentTree, left, right int) {
+		*node = LazySegmentTree{
+			left:  left,
+			right: right,
+			lo:    &LazySegmentTree{},
+			ro:    &LazySegmentTree{},
 		}
 		if left == right {
-			st.sum = nums[left-1]
+			node.sum = nums[left-1]
 			return
 		}
 
 		mid := (left + right) / 2
-		dfs(st.lChild, left, mid)
-		dfs(st.rChild, mid+1, right)
-		st.sum = st.lChild.sum + st.rChild.sum
+		dfs(node.lo, left, mid)
+		dfs(node.ro, mid+1, right)
+		node.sum = node.lo.sum + node.ro.sum
 	}
 
 	// 线段树的下标一般从 1 开始，即 [1, len(nums)]
@@ -28,107 +28,126 @@ func NewLazySegmentTree(nums []int) *LazySegmentTree {
 	return root
 }
 
+// NewLazySegmentTree 初始化线段树节点
+func NewLazySegmentTree(left, right int) *LazySegmentTree {
+	return &LazySegmentTree{
+		left:  left,
+		right: right,
+
+		sum: 0, // 根据题目要求修改
+
+		lo: nil,
+		ro: nil,
+
+		todoAdd:    0,
+		todoUpdate: nil,
+	}
+}
+
 // LazySegmentTree
 // 动态开点线段树·其二·延迟标记（区间修改 + 区间覆盖）
 // 同时支持区间修改和区间覆盖的，会比只支持一种的繁琐一些。
 type LazySegmentTree struct {
-	left, right, sum int
+	left, right int // 该点的区间范围：[left,right]
 
-	lChild, rChild *LazySegmentTree
+	sum int // 根据题目要求，可能求：区间最值 / 区间和
+
+	lo, ro *LazySegmentTree // 左右儿子节点
 
 	todoAdd    int  // 区间增加延迟标记
 	todoUpdate *int // 区间覆盖延迟标记
 }
 
-func (st *LazySegmentTree) Query(left, right int) int {
+func (t *LazySegmentTree) Query(left, right int) int {
 	// 对于不在线段树中的点，应按照题意来返回
-	if st == nil || left > st.right || right < st.left {
+	if t == nil || left > t.right || right < t.left {
 		return 0
 	}
-	if left <= st.left && st.right <= right {
-		return st.sum
+	if left <= t.left && t.right <= right {
+		return t.sum
 	}
 
-	st.spread()
-	return st.lChild.Query(left, right) + st.rChild.Query(left, right)
+	t.spread()
+	return t.lo.Query(left, right) + t.ro.Query(left, right)
 }
 
-// 也常叫做 pushdown
+// 也常叫做 push down
 // 1. 动态开点
 // 2. 把自身的 lazy 标记传递给子节点，并清除自身的 lazy 标记
-func (st *LazySegmentTree) spread() {
-	if st.left == st.right {
+func (t *LazySegmentTree) spread() {
+	if t.left == t.right {
 		return
 	}
 
-	mid := (st.left + st.right) / 2
-	if st.lChild == nil {
-		st.lChild = &LazySegmentTree{left: st.left, right: mid}
+	mid := (t.left + t.right) / 2
+	if t.lo == nil {
+		t.lo = NewLazySegmentTree(t.left, mid)
 	}
-	if st.rChild == nil {
-		st.rChild = &LazySegmentTree{left: mid + 1, right: st.right}
+	if t.ro == nil {
+		t.ro = NewLazySegmentTree(mid+1, t.right)
 	}
 
 	// 1. 必须先传递区间覆盖的 lazy 标记给子节点
-	if st.todoUpdate != nil {
-		st.lChild.doUpdate(*st.todoUpdate)
-		st.rChild.doUpdate(*st.todoUpdate)
-		st.todoUpdate = nil
+	if t.todoUpdate != nil {
+		t.lo.doUpdate(*t.todoUpdate)
+		t.ro.doUpdate(*t.todoUpdate)
+		t.todoUpdate = nil
 	}
 
 	// 2. 再处理区间增加的 lazy 标记
-	if st.todoAdd != 0 {
-		st.lChild.doAdd(st.todoAdd)
-		st.rChild.doAdd(st.todoAdd)
-		st.todoAdd = 0
+	if t.todoAdd != 0 {
+		t.lo.doAdd(t.todoAdd)
+		t.ro.doAdd(t.todoAdd)
+		t.todoAdd = 0
 	}
-
 }
 
 // 更新本区间结点的值，并将值保存为 lazy 标记，等到需要更新子节点时，再下放 lazy 标记
-func (st *LazySegmentTree) doUpdate(value int) {
-	st.todoAdd = 0
-	st.todoUpdate = &value
-	st.sum = value * (st.right - st.left + 1)
+func (t *LazySegmentTree) doUpdate(value int) {
+	t.todoAdd = 0
+	t.todoUpdate = &value
+	t.sum = value * (t.right - t.left + 1)
+
+	// t.mx = max(t.mx, value) // 更新最值
 }
 
 // 更新本区间结点的值，并将值保存为 lazy 标记，等到需要更新子节点时，再下放 lazy 标记
-func (st *LazySegmentTree) doAdd(add int) {
-	st.todoAdd += add
-	st.sum += add * (st.right - st.left + 1)
+func (t *LazySegmentTree) doAdd(add int) {
+	t.todoAdd += add
+	t.sum += add * (t.right - t.left + 1)
 }
 
-func (st *LazySegmentTree) operator(left, right int, op func(node *LazySegmentTree)) {
+func (t *LazySegmentTree) operator(left, right int, op func(node *LazySegmentTree)) {
 	// 当前节点已被区间 [left, right] 完整覆盖，不再继续递归
-	if left <= st.left && st.right <= right {
-		op(st)
+	if left <= t.left && t.right <= right {
+		op(t)
 		return
 	}
 
-	st.spread()
+	t.spread()
 
-	mid := (st.left + st.right) / 2
+	mid := (t.left + t.right) / 2
 	if left <= mid {
-		st.lChild.operator(left, right, op)
+		t.lo.operator(left, right, op)
 	}
 	if mid < right {
-		st.rChild.operator(left, right, op)
+		t.ro.operator(left, right, op)
 	}
-	st.sum = st.lChild.sum + st.rChild.sum
+	t.sum = t.lo.sum + t.ro.sum // 根据题目要求修改
 }
 
 // Update 区间覆盖
 // 把区间 [left, right] 的值都修改为 value
-func (st *LazySegmentTree) Update(left, right int, value int) {
-	st.operator(left, right, func(node *LazySegmentTree) {
+func (t *LazySegmentTree) Update(left, right int, value int) {
+	t.operator(left, right, func(node *LazySegmentTree) {
 		node.doUpdate(value)
 	})
 }
 
 // Add 区间增加
 // 区间 [left, right] 的值都加上 add
-func (st *LazySegmentTree) Add(left, right int, add int) {
-	st.operator(left, right, func(node *LazySegmentTree) {
+func (t *LazySegmentTree) Add(left, right int, add int) {
+	t.operator(left, right, func(node *LazySegmentTree) {
 		node.doAdd(add)
 	})
 }
--- container/seg_tree/segment_tree.go
+++ container/seg_tree/segment_tree.go
@@ -1,26 +1,26 @@
 package seg_tree
 
-// NewSegmentTree
+// NewSegmentTreeWithNums
 // 根据数组初始化线段树
-func NewSegmentTree(nums []int) *SegmentTree {
+func NewSegmentTreeWithNums(nums []int) *SegmentTree {
 	root := &SegmentTree{}
-	var dfs func(st *SegmentTree, left, right int)
-	dfs = func(st *SegmentTree, left, right int) {
-		*st = SegmentTree{
-			left:   left,
-			right:  right,
-			lChild: &SegmentTree{},
-			rChild: &SegmentTree{},
+	var dfs func(node *SegmentTree, left, right int)
+	dfs = func(node *SegmentTree, left, right int) {
+		*node = SegmentTree{
+			left:  left,
+			right: right,
+			lo:    &SegmentTree{},
+			ro:    &SegmentTree{},
 		}
 		if left == right {
-			st.sum = nums[left-1]
+			node.sum = nums[left-1]
 			return
 		}
 
 		mid := (left + right) / 2
-		dfs(st.lChild, left, mid)
-		dfs(st.rChild, mid+1, right)
-		st.sum = st.lChild.sum + st.rChild.sum
+		dfs(node.lo, left, mid)
+		dfs(node.ro, mid+1, right)
+		node.sum = node.lo.sum + node.ro.sum
 	}
 
 	// 线段树的下标一般从 1 开始，即 [1, len(nums)]
@@ -28,61 +28,79 @@ func NewSegmentTree(nums []int) *SegmentTree {
 	return root
 }
 
+// NewSegmentTree 初始化线段树
+func NewSegmentTree(left, right int) *SegmentTree {
+	return &SegmentTree{
+		left:  left,
+		right: right,
+
+		sum: 0, // 根据题目要求修改
+
+		lo: nil,
+		ro: nil,
+	}
+}
+
 // SegmentTree
 // 动态开点线段树·其一·单点修改
 type SegmentTree struct {
-	left, right, sum int
+	left, right int // 该点的区间范围：[left,right]
 
-	lChild, rChild *SegmentTree
+	sum int // 根据题目要求，可能求：区间最值 / 区间和
+
+	lo, ro *SegmentTree // 左右儿子节点
 }
 
-func (st *SegmentTree) Query(left, right int) int {
-	if st == nil || left > st.right || right < st.left {
+func (t *SegmentTree) Query(left, right int) int {
+	if t == nil || left > t.right || right < t.left {
 		return 0
 	}
-	if left <= st.left && st.right <= right {
-		return st.sum
+	if left <= t.left && t.right <= right {
+		return t.sum
 	}
-	return st.lChild.Query(left, right) + st.rChild.Query(left, right)
+	return t.lo.Query(left, right) + t.ro.Query(left, right)
 }
 
+// 也常叫做 push down
 // 动态开点
-func (st *SegmentTree) spread() {
-	mid := (st.left + st.right) / 2
-	if st.lChild == nil {
-		st.lChild = &SegmentTree{left: st.left, right: mid}
+func (t *SegmentTree) spread() {
+	mid := (t.left + t.right) / 2
+	if t.lo == nil {
+		t.lo = NewSegmentTree(t.left, mid)
 	}
-	if st.rChild == nil {
-		st.rChild = &SegmentTree{left: mid + 1, right: st.right}
+	if t.ro == nil {
+		t.ro = NewSegmentTree(mid+1, t.right)
 	}
 }
 
-// 单点修改，没有延迟操作（只有区间修改需要延迟操作），因此可以整合 Add, Update 操作
-func (st *SegmentTree) operator(i int, op func(st *SegmentTree)) {
-	if st.left == st.right {
-		op(st)
+// 单点修改，没有延迟操作（只有区间修改需要延迟操作）
+// 用于整合 Add, Update 操作
+func (t *SegmentTree) operator(i int, op func(node *SegmentTree)) {
+	if t.left == t.right {
+		op(t)
 		return
 	}
 
-	st.spread() // 动态开点
+	t.spread() // 动态开点
 
-	mid := (st.left + st.right) / 2
+	mid := (t.left + t.right) / 2
 	if i <= mid {
-		st.lChild.operator(i, op)
+		t.lo.operator(i, op)
 	} else {
-		st.rChild.operator(i, op)
+		t.ro.operator(i, op)
 	}
-	st.sum = st.lChild.sum + st.rChild.sum
+	t.sum = t.lo.sum + t.ro.sum // 根据题目要求修改
 }
 
-func (st *SegmentTree) Update(i int, value int) {
-	st.operator(i, func(st *SegmentTree) {
-		st.sum = value
+func (t *SegmentTree) Update(i int, value int) {
+	t.operator(i, func(node *SegmentTree) {
+		node.sum = value // 根据题目要求修改
+		// node.mx = max(node.mx, value) // 最值
 	})
 }
 
-func (st *SegmentTree) Add(i int, add int) {
-	st.operator(i, func(st *SegmentTree) {
-		st.sum += add
+func (t *SegmentTree) Add(i int, add int) {
+	t.operator(i, func(node *SegmentTree) {
+		node.sum += add
 	})
 }
